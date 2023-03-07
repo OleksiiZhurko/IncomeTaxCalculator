9a5b64cac16c39ac5eb7b06cd320848492b7031c
OleksiiZhurko <alexey.zhurko0@gmail.com>
2023-03-07 20:09:26 +0200

    FIX - Updated project structure
    (cherry picked from commit 9c0712b26bc4c660a38b3d11c9de2b2f788fc395)

---
+++ src/main/java/com/incometaxcalculator/data/io/TXTFileReader.java
@@ -0,0 +1,38 @@
+package com.incometaxcalculator.data.io;
+
+import com.incometaxcalculator.exceptions.WrongFileFormatException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class TXTFileReader extends FileReader {
+
+  protected int checkForReceipt(BufferedReader inputStream)
+      throws NumberFormatException, IOException {
+    String line;
+    while (!isEmpty(line = inputStream.readLine())) {
+      String values[] = line.split(" ", 3);
+      if (values[0].equals("Receipt")) {
+        if (values[1].equals("ID:")) {
+          int receiptId = Integer.parseInt(values[2].trim());
+          return receiptId;
+        }
+      }
+    }
+    return -1;
+  }
+
+  protected String getValueOfField(String fieldsLine) throws WrongFileFormatException {
+    if (isEmpty(fieldsLine)) {
+      throw new WrongFileFormatException();
+    }
+    try {
+      String values[] = fieldsLine.split(" ", 2);
+      values[1] = values[1].trim();
+      return values[1];
+    } catch (NullPointerException e) {
+      throw new WrongFileFormatException();
+    }
+  }
+
+}
---
+++ src/main/java/com/incometaxcalculator/data/io/XMLFileReader.java
@@ -0,0 +1,37 @@
+package com.incometaxcalculator.data.io;
+
+import com.incometaxcalculator.exceptions.WrongFileFormatException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class XMLFileReader extends FileReader {
+
+  protected int checkForReceipt(BufferedReader inputStream)
+      throws NumberFormatException, IOException {
+    String line;
+    while (!isEmpty(line = inputStream.readLine())) {
+      String values[] = line.split(" ", 3);
+      if (values[0].equals("<ReceiptID>")) {
+        int receiptId = Integer.parseInt(values[1].trim());
+        return receiptId;
+      }
+    }
+    return -1;
+  }
+
+  protected String getValueOfField(String fieldsLine) throws WrongFileFormatException {
+    if (isEmpty(fieldsLine)) {
+      throw new WrongFileFormatException();
+    }
+    try {
+      String valueWithTail[] = fieldsLine.split(" ", 2);
+      String valueReversed[] = new StringBuilder(valueWithTail[1]).reverse().toString().trim()
+          .split(" ", 2);
+      return new StringBuilder(valueReversed[1]).reverse().toString();
+    } catch (NullPointerException e) {
+      throw new WrongFileFormatException();
+    }
+  }
+
+}
